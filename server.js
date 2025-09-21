c7f7c8798de20b87c5829b53d4131ff5491ede6b
Amal <amalmgafoor546@gmail.com>
2025-09-21 15:41:23 +0530

    hello

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running at http://0.0.0.0:${PORT}`);
+server.listen(PORT,  () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
